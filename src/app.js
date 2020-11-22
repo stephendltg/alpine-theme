e70e98bba5478963162be4d198f09a94847388fc
stephendltg <s.deletang@laposte.net>
2020-11-22 18:53:49 +0100

    inject layer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import { registerComponents } from './utils.js'
+import { registerComponents, registerLayouts } from './utils.js'
 import './store.js'
 
 const components = [...document.querySelectorAll("[x-data$='()']")].map(component => {
   return component.getAttribute('x-data').slice(0,-2)
 })
+
+const layouts = [...document.querySelectorAll("[x-src]")].map(layer => layer.getAttribute('x-src'))
 
 ;(async () => {
   /*
@@ -11,8 +13,17 @@ const components = [...document.querySelectorAll("[x-data$='()']")].map(componen
     toggle: './components/toggle.js'
   })
   */
+  /*
+  fetch('./layouts/header.html?nocache='+ Math.random())
+  .then(response => response.text())
+  .then(data => document.querySelector("header").innerHTML = data );
+  */
+  
+  
+  await registerLayouts(layouts)
   await registerComponents(components)
 
   await import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
   await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')  
 })()
+  
--- src/utils.js
+++ src/utils.js
@@ -21,3 +21,63 @@ export const registerComponents = components => {
     })
   )
 }
+
+export const registerLayouts = layouts => {
+  let layers = []
+  return Promise.all( 
+    layouts.map(url => 
+      _fetch(url)
+      .then(res => res.text())
+      .then(data => 
+        layers.push({
+          url: url,
+          data: data
+        }) 
+      ) 
+    )
+  )
+  .then(data => layers.map( x => {
+      [...document.querySelectorAll("[x-src='"+x.url+"']")].map(e => e.innerHTML = x.data )
+    })
+  )
+}
+
+
+export const _fetch = ( uri, options = {}, timeout = 1000 ) => {
+  const req = fetch( uri, options );
+  const TIMEOUT = new Promise((resolve, reject) => {
+   return setTimeout(() => reject(new Error('request timeout')), timeout );
+  })
+  return Promise.race([req, TIMEOUT]).then(response => response);
+}
+
+
+export const loadScript = src => {
+  return new Promise( function (resolve, reject) {
+    if( document.querySelector('script[src="'+src+'"]') ){
+      resolve();
+      return;
+    }
+    let el = document.createElement('script')
+    el.type = 'text/javascript'
+    el.async = true
+    el.src = src
+    el.addEventListener('load',  resolve)
+    el.addEventListener('error', reject)
+    el.addEventListener('abort', reject)
+    document.head.appendChild(el)
+  })
+}
+
+export const unloadScript = src => {
+  return new Promise( function (resolve, reject) {
+    const el = document.querySelector('script[src="'+src+'"]')
+    if(!el){
+      reject()
+      return
+    }
+    document.head.removeChild(el);
+    resolve();
+  })
+}
+
