2c694c41ff918ef0dd67a4c501deda46dbcc1c71
stephendltg <s.deletang@laposte.net>
2020-11-26 23:41:36 +0100

    add material design js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,6 +1,20 @@
-import { registerComponents, registerLayouts, buildComponent } from './utils.js'
+import { registerComponents, registerLayouts, buildComponent, loadScript} from './utils.js'
 import ("https://cdn.jsdelivr.net/npm/@ryangjchandler/spruce@2.x.x/dist/spruce.umd.js")
 import ('./store.js').then(module => window[component] = module.default)
+
+/* 
+ * materialdesign js
+ */
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js')
+.then( e => {
+  
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+})
 
 const components = [...document.querySelectorAll("[x-data$='()']")].map(component => {
   return component.getAttribute('x-data').slice(0,-2)
@@ -24,4 +38,3 @@ const layouts = [...document.querySelectorAll("[x-src]")].map(layer => layer.get
   await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
   
 })()
-
