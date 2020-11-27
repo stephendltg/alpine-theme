2f5ae3d167046a2a24b995bcf0301ccde89ef465
stephendltg <s.deletang@laposte.net>
2020-11-27 19:10:55 +0100

    move load materialize.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,17 +6,6 @@
  */
 import { registerComponents, registerLayouts, loadScript } from './utils.js'
 
-/* 
- * materialdesign js
- *
- */
-loadScript('./static/js/vendors/materialize.min.js')
-.then( e => {
-  console.log('materialize loaded')
-})
-.catch( e =>
-  console.warn('error: materialize  unloaded')
-)
 
 const components = [...document.querySelectorAll("[x-data$='()']")].map(component => {
   return component.getAttribute('x-data').slice(0,-2)
@@ -25,13 +14,17 @@ const components = [...document.querySelectorAll("[x-data$='()']")].map(componen
 const layouts = [...document.querySelectorAll("[x-src]")].map(layer => layer.getAttribute('x-src'))
 
 
-
 ;(async () => {
   
   await import ('./vendors/spruce.js')
   await import ('./store.js')
+  
   await registerLayouts(layouts)
   await registerComponents(components)
+  
+  loadScript('./static/js/vendors/materialize.min.js')
+  .then( e => console.log('materialize loaded') )
+  .catch( e => console.warn('error: materialize  unloaded') )
 
   await import('./vendors/alpine-magic-helper.js')
   await import('./vendors/alpine.js')
