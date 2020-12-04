f3fa83460f9c1c66bb8bd05941108855821c640c
stephendltg <s.deletang@laposte.net>
2020-12-04 18:26:34 +0100

    rebuild

--- static/js/app.js
+++ static/js/app.js
@@ -23,13 +23,26 @@ const layouts = [...document.querySelectorAll("[x-src]")].map(layer => layer.get
   await registerComponents(components)
   
   loadScript('./static/js/vendors/materialize.min.js')
-  .then( e => console.log('materialize loaded') )
+  .then( e => {
+    // console.log('materialize loaded')
+    // init
+    M.AutoInit();
+    // Element
+    var elems = document.querySelectorAll('.sidenav-trigger');
+    var instances = M.Sidenav.init(elems);
+    // Floating button
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+    // Date picker
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+  })
   .catch( e => console.warn('error: materialize  unloaded') )
 
   await import('./vendors/alpine-magic-helper.js')
   await import('./vendors/alpine.js')
-
-  await import('./material.js')
   
 })()
 
--- static/js/store.js
+++ static/js/store.js
@@ -6,24 +6,6 @@
  * Version: 1.0.0
  */
 
-Spruce.store('counter', {
-  count: 0,
-  countUp: 0,
-  countDown: 100
-})
-
-
-Spruce.store('toast', {
-    toasts: ["<i>test</i>"],
-    add(message) {
-        this.toasts.push(message)
-    },
-    remove(index) {
-        this.toasts.splice(index, 1)
-    }
-} )
-
-
 Spruce.store('user', {
     name: 'stephendtlg',
     email: 's.deletang@icloud.com',
